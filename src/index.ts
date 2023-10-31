af0349d38638ea003e777760a1035efe399698f3
Kamarotto <99406638+Kamarotto@users.noreply.github.com>
2023-10-31 10:15:41 -0400

    varios bglh

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,47 @@
-import express from 'express';
+import express, {Request} from 'express';
 
 // Cria uma instância do aplicativo Express
 const app = express();
+app.use(express.json())
 
 type Filme = {
     id: number,
     titulo: string,
-    descricao: string, 
+    descricao: string,
     foto: string,
 }
-const Filmes_repositorio:Filme[] = []
+let filmes_repositorio:Filme[] = []
 
 
 // Define uma rota padrão
 app.get('/filmes/:id', (req, res) => {
-    
-    const id = parseInt (req.params.id)
-    const filme = Filmes_repositorio.find(filme => filme.id === id)
-    if (filme) (
-        res.send(filme)
-    ) else (
-        res.status(404).send()
-    )
-
-    res.send([filme]);
+    const id = parseInt(req.params.id)
+    const filme = filmes_repositorio.find(filme => filme.id === id)
+    if (!filme) res.status(404).send()
+    res.send(filme)        
 });
 
-app.post('/filmes', (req, res) => {
-
-    const filme = req.body
-    Filmes_repositorio.push(filme)
+app.post('/filmes', (req:Request, res) => {
+    const {id, titulo, descricao, foto} = req.body
+    const filme:Filme = {
+        id,
+        titulo,
+        descricao,
+        foto,
+    }
+    filmes_repositorio.push(filme)
     res.status(201).send(filme)
-    
 });
+
+app.delete('/filmes/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const filme = filmes_repositorio.find(filme => filme.id === id)
+    if (!filme) return res.status(404).send(filme)
+    const filterFilme = filmes_repositorio.filter(filme => filme.id !== id)
+    filmes_repositorio = filterFilme
+    res.status(200).send(filme)
+});
+
 
 // Inicia o servidor
 app.listen(3000, () => {
