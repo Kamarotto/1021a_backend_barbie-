82bd8154f51f8cfa7365010885cd6f8e0a9cb939
Kamarotto <99406638+Kamarotto@users.noreply.github.com>
2023-11-08 08:27:55 -0400

    erros do tere

--- src/infra/banco/banco-mongodb.test.ts
+++ src/infra/banco/banco-mongodb.test.ts
@@ -1,16 +1,16 @@
 import {describe,test,expect} from 'vitest'
 import BancoMongoDB from './banco-mongodb'
 
-describe('Banco em memória', () =>{
-  test('Deve salvar no banco em memoria',async () => {
+describe('Banco MongoDB', () =>{
+  test('Deve salvar no banco MongoDB',async () => {
     const input = {
         id:1,
         titulo:'test',
         descricao:'test',
         foto:'test',
     }
-    const BancoMongoDB = new BancoMongoDB()
-    const result = await BancoMongoDB.salvar(input)
+    const bancoMongoDB = new BancoMongoDB()
+    const result = await bancoMongoDB.salvar(input)
     expect(result).toBe(true)
 })
 })
